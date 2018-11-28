ef414a3be444c4d886ad4c246c5d3b22740f0ce4
chenfeihao <chenfeihao@corp.netease.com>
2018-11-28 16:05:28 +0800

    @dev 初始化工程

---
+++ src/main/java/com/cfh/cacheframe/CacheFrameApplication.java
@@ -0,0 +1,12 @@
+package com.cfh.cacheframe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CacheFrameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CacheFrameApplication.class, args);
+    }
+}
