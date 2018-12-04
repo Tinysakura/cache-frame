e454a3522594733263b1ca04ec282c2f36475679
chenfeihao <chenfeihao@corp.netease.com>
2018-12-04 17:27:02 +0800

    @dev FIFO与LRU 缓存容器设计

---
+++ src/main/java/com/cfh/cacheframe/adapter/client/FIFOCacheClient.java
@@ -0,0 +1,12 @@
+package com.cfh.cacheframe.adapter.client;
+
+import com.cfh.cacheframe.adapter.CacheClient;
+
+/**
+ * @Author: chenfeihao@corp.netease.com
+ * @Date: 2018/12/4
+ * @TODO 先进先出的缓存容器
+ */
+
+public class FIFOCacheClient implements CacheClient {
+}
---
+++ src/main/java/com/cfh/cacheframe/adapter/client/LFUCacheClient.java
@@ -0,0 +1,12 @@
+package com.cfh.cacheframe.adapter.client;
+
+import com.cfh.cacheframe.adapter.CacheClient;
+
+/**
+ * @Author: chenfeihao@corp.netease.com
+ * @Date: 2018/12/4
+ * @TODO 最不常使用的元素被淘汰的缓存容器
+ */
+
+public class LFUCacheClient implements CacheClient {
+}
